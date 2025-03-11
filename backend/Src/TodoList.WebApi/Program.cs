9e43817aca78128be8dd8ca22e6db86bec1ad679
kubackikacper <kacper1048@gmail.com>
2025-03-11 15:41:32 +0100

    working on xunit tests

--- backend/Src/TodoList.WebApi/Program.cs
+++ backend/Src/TodoList.WebApi/Program.cs
@@ -1,45 +1,45 @@
 using Microsoft.EntityFrameworkCore;
 using TodoList.WebApi.Data;
 using TodoList.WebApi.Services;
-namespace TodoList.WebApi
+
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite("Data Source = ToDoListAssignment.db"));
+
+builder.Services.AddScoped<IToDoService, ToDoService>();
+builder.Services.AddCors(c =>
 {
-    public class Program
+    c.AddPolicy(name: "CorsPolicy", builder =>
     {
+        builder.WithOrigins("http://localhost:3000").AllowAnyHeader().AllowAnyMethod();
+    });
+});
+var app = builder.Build();
+app.UseStaticFiles();
+app.UseRouting();
 
-        public static void Main(string[] args) 
-        {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-            builder.Services.AddControllers();
-            builder.Services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlite("Data Source = ToDoListAssignment.db"));
-
-            builder.Services.AddScoped<IToDoService, ToDoService>();
-            var app = builder.Build();
-            app.UseCors(policy => policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
-            app.UseStaticFiles();
-            app.UseRouting();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-            app.MapControllers();
-            app.Run();
-        }
-    }
-    
-
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+app.UseCors("CorsPolicy");
+app.MapControllers();
+app.Run();
+
+public partial class Program { }
 
 
 
